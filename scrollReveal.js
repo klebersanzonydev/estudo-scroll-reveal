791bf30ac2472328bac48fb14d1acf7f6d6ab1e0
Kleber Sanzony <ks@MacBook-Pro-de-Kleber-2.local>
2023-04-23 15:24:47 -0300

    new features

--- scrollReveal.js
+++ scrollReveal.js
@@ -2,28 +2,35 @@ window.sr = ScrollReveal({ reset: true })
 
 ScrollReveal().reveal('.area-1', {
     delay: 500,
+    duration: 2000,
+    interval: 200,
+    easing: 'ease-in-out',
     rotate:{
         x: 0,
         y: 0,
         z: 0
     },
-    scale: 2,
+    scale: 0.8,
+    reset: true
     
 })
 ScrollReveal().reveal('.area-2', {
-    delay: 900,
-    rotate:{
-        x: 100,
-        y: 0,
-        z: 0
-    },
-    scale: 1
+    interval: 200, 
+    duration: 2000,
+    easing: 'ease-in',
+    distance: '150%',
+    origin: 'left',
+    opacity: 0.8
 })
 ScrollReveal().reveal('.area-3', {
     delay: 900,
+    duration: 2000,
+    interval: 200,
+    
     rotate:{
         x: 0,
         y: 100,
         z: 50
     }
+
 })
