1156f6d7dfe605940aecb83b1ecde7bf0c3a8ebe
Kleber Sanzony <ks@MacBook-Pro-de-Kleber-2.local>
2023-04-23 14:19:37 -0300

    estudo scroll-reveal

---
+++ scrollReveal.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.area-1', {
+    delay: 500,
+    rotate:{
+        x: 0,
+        y: 0,
+        z: 0
+    },
+    scale: 2,
+    
+})
+ScrollReveal().reveal('.area-2', {
+    delay: 900,
+    rotate:{
+        x: 100,
+        y: 0,
+        z: 0
+    },
+    scale: 1
+})
+ScrollReveal().reveal('.area-3', {
+    delay: 900,
+    rotate:{
+        x: 0,
+        y: 100,
+        z: 50
+    }
+})
